2b64d5bc69bb7fa25434ae9ff985e01c69ee2514
david kayembe <59444886+davk2020@users.noreply.github.com>
2021-03-06 11:09:57 +0100

    getting started complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const app = express();
+app.use('/static', express.static('public'));
 const port = 4500;
 
 app.get('/', (req, res) => res.json({ text: "hello dude" }));
